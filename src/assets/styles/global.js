eeee9ebd7164085af252ad0b700fa14134072441
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-08-10 01:02:29 -0300

    feat: :sparkles: Add styles default

--- src/assets/styles/global.js
+++ src/assets/styles/global.js
@@ -8,6 +8,14 @@ export default createGlobalStyle`
   }
 
   body {
+    background: ${({ theme }) => theme.colors.gray[900]};
+    color: ${({ theme }) => theme.colors.gray[300]};
+    -webkit-font-smoothing: antialiased;
+  }
 
+  body, input, text-area, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
   }
 `;
